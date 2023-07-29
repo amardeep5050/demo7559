4e89713424d0b2692ab52ad8acd933f6b21997d7
amardeep5050 <amardeepkhade50@gmail.com>
2023-07-29 19:55:04 +0530

    clean

--- c.java
+++ c.java
@@ -1,19 +1,9 @@
 public class C{
 
+
   public static void main(String[] args) {
      System.out.println("branch C");
      System.out.println("branch Dgti ");
-    int first = 10;
-    int second = 20;
-
-    // add two numbers
-    int sum = first + second;
-    System.out.println(first + " + " + second + " = "  + sum);
-  }
-}public class C{
-  public static void main(String[] args) {
-    
-
     int first = 10;
     int second = 20;
 
