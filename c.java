d3ac5af4a48789a68ccd8a4eb7f6b06b7f2439bb
amardeep5050 <amardeepkhade50@gmail.com>
2023-07-29 18:55:58 +0530

    G

--- c.java
+++ c.java
@@ -10,4 +10,15 @@ public class C{
     int sum = first + second;
     System.out.println(first + " + " + second + " = "  + sum);
   }
+}public class C{
+  public static void main(String[] args) {
+    
+
+    int first = 10;
+    int second = 20;
+
+    // add two numbers
+    int sum = first + second;
+    System.out.println(first + " + " + second + " = "  + sum);
+  }
 }
