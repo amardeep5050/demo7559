9242b2231e82fd77cdd3bb72e9d91a8446cf23e5
amardeep5050 <amardeepkhade50@gmail.com>
2023-08-01 11:31:13 +0530

    new upadte

---
+++ D.java
@@ -0,0 +1,11 @@
+ public static void main(String[] args) {
+System.out.println("branch D ");
+     System.out.println("branch d");
+     System.out.println("branch Dgti ");
+    int first = 10;
+    int second = 20;
+
+    // add two numbers
+    int sum = first + second;
+    System.out.println(first + " + " + second + " = "  + sum);
+  }
